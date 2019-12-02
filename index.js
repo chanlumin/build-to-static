723bd0e7b21825ec3b65f3e48b3ae9bd32a7bf8b
chenlumin3 <chenlumin3@jd.com>
2019-12-02 16:49:51 +0800

    更新静态资源

---
+++ index.js
@@ -0,0 +1,9 @@
+// const shell = require('shelljs')
+const shell = require('shelljs')
+if(!shell.which('git')) {
+  shell.echo('require git')
+  shell.exit('1')
+}
+shell.exec('git init')
+shell.exec('git add .')
+shell.exec('git commit -am "更新静态资源"')
