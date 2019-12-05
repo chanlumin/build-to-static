f2f2f0b33bb91e695499594e9d831de212f78fd3
mlc <1025300632@qq.com>
2019-12-05 10:10:04 +0800

    feat: for push

--- src/add.js
+++ src/add.js
@@ -3,4 +3,5 @@ const shell = require('shelljs')
 // shell.exec(`git add ${}`)
 console.log(shell.pwd().stdout)
 
-shell.exec(`git add ${shell.pwd().stdout}`)
+let r = shell.exec(`git add ${shell.pwd().stdout}`)
+console.log(r.code)
